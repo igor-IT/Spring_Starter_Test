1499edd3ea3eb934fc1a314827ac5b759c6bde57
igor-IT <58485359+igor-IT@users.noreply.github.com>
2020-11-16 10:44:07 +0200

    Verion 2

--- src/main/java/com/example/demo/InjectByTypeBeanPostProcessor.java
+++ src/main/java/com/example/demo/InjectByTypeBeanPostProcessor.java
@@ -1,53 +1,34 @@
 package com.example.demo;
 
 import lombok.SneakyThrows;
-import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.ApplicationContext;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class InjectByTypeBeanPostProcessor<T> implements BeanPostProcessor, ApplicationListener<ContextRefreshedEvent> {
-    private Map<Class<?>,Object> beanHolder = new HashMap<>();
-    private List<Object> annotaitedBeans = new ArrayList<>();
+public class InjectByTypeBeanPostProcessor<T> implements BeanPostProcessor {
+    @Autowired
+    private  ApplicationContext context;
 
     @SneakyThrows
     @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
         Field[] declaredFields = bean.getClass().getFields();
-        beanHolder.put(bean.getClass(),bean);
+        List<Object> list = new ArrayList<>();
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(InjectByList.class)) {
-                annotaitedBeans.add(bean);
-            }
-        }
-        return bean;
-    }
-    @SneakyThrows
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-       annotaitedBeans.stream().forEach(bean->fieldInjection(bean));
-    }
-    @SneakyThrows
-    private void fieldInjection(Object beanObj){
-        Field[] declaredFields = beanObj.getClass().getDeclaredFields();
-        //Проблема с этим листом, я не нашел как из поля получить дженерик тайп, я знаю что это ошибка но я старался как мог
-        List<Object> list = new ArrayList<>();
-        for (Field field : declaredFields){
-            if (field.isAnnotationPresent(InjectByList.class)){
                 InjectByList annotation = field.getAnnotation(InjectByList.class);
                 Class<?>[] classes = annotation.classes();
                 for (Class<?> aClass : classes) {
-                    list.add(beanHolder.get(aClass));
+                    list.add(context.getBean(aClass));
                 }
                 field.setAccessible(true);
-                field.set(beanObj,list);
+                field.set(bean,list);
             }
         }
+        return bean;
     }
 }
