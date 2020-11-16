913e5ced211c69f52be7f2a74829ce30b6272a8b
igor-IT <58485359+igor-IT@users.noreply.github.com>
2020-11-16 18:41:57 +0200

    Add Injection to setter method

--- src/main/java/com/example/demo/InjectByList.java
+++ src/main/java/com/example/demo/InjectByList.java
@@ -3,10 +3,13 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Import;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD,ElementType.FIELD})
 @Import(InjectByTypeBeanPostProcessor.class)
 public  @interface  InjectByList {
     Class<?>[] classes();
--- src/main/java/com/example/demo/InjectByTypeBeanPostProcessor.java
+++ src/main/java/com/example/demo/InjectByTypeBeanPostProcessor.java
@@ -4,12 +4,9 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.*;
 
 public class InjectByTypeBeanPostProcessor implements BeanPostProcessor {
@@ -20,6 +17,23 @@ public class InjectByTypeBeanPostProcessor implements BeanPostProcessor {
     public Object postProcessAfterInitialization(Object bean, String beanName) {
         Field[] declaredFields = bean.getClass().getDeclaredFields();
         Collection<Object> list ;
+        Method[] declaredMethods = bean.getClass().getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if (method.isAnnotationPresent(InjectByList.class)) {
+                if (method.getName().startsWith("set")) {
+                    InjectByList annotation = method.getAnnotation(InjectByList.class);
+                    Class<?>[] classes = annotation.classes();
+                    Class<?> parameterType = method.getParameterTypes()[0];
+                    Collection<Object> beans = getCollection(parameterType.getSimpleName());
+                    for (Class<?> aClass : classes) {
+                        beans.add(context.getBean(aClass));
+                    }
+                    method.invoke(bean, beans);
+                }else {
+                    throw new IllegalArgumentException("Аннотация работает только для сеттеров и филдов");
+                }
+            }
+        }
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(InjectByList.class)) {
                 InjectByList annotation = field.getAnnotation(InjectByList.class);
