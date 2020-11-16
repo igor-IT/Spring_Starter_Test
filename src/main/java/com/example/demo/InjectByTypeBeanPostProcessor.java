69bc98cb45ae551007e835707c3fb09fcd5e5978
igor-IT <58485359+igor-IT@users.noreply.github.com>
2020-11-16 13:52:14 +0200

    Injection with BeanFactory

--- src/main/java/com/example/demo/InjectByTypeBeanPostProcessor.java
+++ src/main/java/com/example/demo/InjectByTypeBeanPostProcessor.java
@@ -4,25 +4,27 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
 
-public class InjectByTypeBeanPostProcessor<T> implements BeanPostProcessor {
+public class InjectByTypeBeanPostProcessor implements BeanPostProcessor {
     @Autowired
     private  BeanFactory context;
-
-
     @SneakyThrows
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) {
-        Field[] declaredFields = bean.getClass().getFields();
-        List<Object> list = new ArrayList<>();
+        Field[] declaredFields = bean.getClass().getDeclaredFields();
+        Collection<Object> list ;
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(InjectByList.class)) {
                 InjectByList annotation = field.getAnnotation(InjectByList.class);
                 Class<?>[] classes = annotation.classes();
+                list = getCollection(field.getType().getSimpleName());
                 for (Class<?> aClass : classes) {
                     list.add(context.getBean(aClass));
                 }
@@ -32,4 +34,12 @@ public class InjectByTypeBeanPostProcessor<T> implements BeanPostProcessor {
         }
         return bean;
     }
+    private Collection<Object> getCollection(String name){
+        if (name.equalsIgnoreCase("List"))
+            return new ArrayList<>();
+        if (name.equalsIgnoreCase("Set"))
+        return new HashSet<>();
+        return new LinkedList<>();
+    }
+
 }
